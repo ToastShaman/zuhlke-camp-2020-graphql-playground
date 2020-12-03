30c9237adb72c9a0f7e59d43e1d51f79e7a9d693
Kevin Denver <kevin.denver@gmail.com>
2020-12-03 18:30:21 +0000

    Add example ProviderTest.java

---
+++ src/test/java/com/github/toastshaman/graphqldemo/ProviderTest.java
@@ -0,0 +1,37 @@
+package com.github.toastshaman.graphqldemo;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@Provider("todo_api")
+@PactFolder("pacts")
+@ExtendWith(PactVerificationInvocationContextProvider.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProviderTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void before(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", port));
+    }
+
+    @TestTemplate
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    @State("an empty repository")
+    public void emptyRepositoryState() {
+    }
+}
